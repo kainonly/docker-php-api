c448ca9a2899aefa6c7208328c18911524b7530c
kain <zhangtqx@vip.qq.com>
2020-07-10 19:11:18 +0800

    update

--- src/DockerClient.php
+++ src/DockerClient.php
@@ -12,22 +12,9 @@ class DockerClient
      * @var ContainerInterface
      */
     private ContainerInterface $container;
-    /**
-     * @var HttpClientInterface
-     */
-    private HttpClientInterface $client;
 
     public function __construct(Client $client)
     {
 
-
-        $this->system = new SystemFactory($client);
-        $this->images = new ImagesFactory($client);
-        $this->containers = new ContainersFactory($client);
-        $this->networks = new NetworksFactory($client);
-        $this->volumes = new VolumesFactory($client);
-        $this->exec = new ExecFactory($client);
-        $this->configs = new ConfigsFactory($client);
-        $this->plugins = new PluginsFactory($client);
     }
 }
